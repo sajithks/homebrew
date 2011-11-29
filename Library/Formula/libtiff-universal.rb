48c50445a9da933e3e8a4e22c1b963bb4abd924b
Thouis (Ray) Jones <thouis@gmail.com>
2011-11-29 15:09:01 +0100

    libtiff universal changes.
    Libtiff's autoconf script tests for various bit widths, totally breaking 32 and 64 bit
    word size calculations.  This commit changes them to use stdint.h, which has most of
    these sizes defined correctly.  Formatting strings are not fixed, and since I'm not
    sure that the required version of autoconf is available, I fix the headers by hand.

--- Library/Formula/libtiff-universal.rb
+++ Library/Formula/libtiff-universal.rb
@@ -9,6 +9,37 @@ class LibtiffUniversal < Formula
     ENV.universal_binary
     # could not get opengl to work, and easiest way to turn it off seems to be to disable X
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--disable-dependency-tracking", "--disable-cxx", "--with-x=no"
+    # I don't want to fight with autconf, especially since libtiff
+    # requires an autoconf version greater than the default on OSX 10.6
+    inreplace 'libtiff/tiffconf.h', '#endif /* _TIFFCONF_ */', DATA.read + '#endif /* _TIFFCONF_ */'
+    File.open("libtiff/tif_config.h", File::WRONLY|File::APPEND) {|f| f.write(DATA.read) }
     system "make install"
   end
 end
+
+__END__
+#ifdef HAVE_STDINT_H
+#undef TIFF_INT16_T
+#undef TIFF_INT32_T
+#undef TIFF_INT64_T
+#undef TIFF_INT8_T
+#undef TIFF_PTRDIFF_T
+#undef TIFF_SSIZE_T
+#undef TIFF_UINT16_T
+#undef TIFF_UINT32_T
+#undef TIFF_UINT64_T
+#undef TIFF_UINT8_T
+
+#include <stdint.h>
+#define TIFF_INT8_T int8_t
+#define TIFF_INT16_T int16_t
+#define TIFF_INT32_T int32_t
+#define TIFF_INT64_T int64_t
+#define TIFF_UINT8_T uint8_t
+#define TIFF_UINT16_T uint16_t
+#define TIFF_UINT32_T uint32_t
+#define TIFF_UINT64_T uint64_t
+#define TIFF_PTRDIFF_T intptr_t
+#define TIFF_SSIZE_T intptr_t
+#endif
+
