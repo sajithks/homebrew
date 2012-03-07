82505d94986d4b4ec434b456fd86ad9aa69f9c93
Thouis (Ray) Jones <thouis@gmail.com>
2012-03-07 10:21:37 +0100

    Move HDF5 prebuilt binaries to github

--- Library/Formula/libhdf5-universal.rb
+++ Library/Formula/libhdf5-universal.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Libhdf5Universal < Formula
-  url "http://dl.dropbox.com/u/16028921/HDF5-Universal.tar.bz2"
+  url "https://github.com/downloads/CellProfiler/CellProfiler/HDF5-Universal.tar.bz2"
   version '1'
   homepage 'http://cellprofiler.org/wiki/'
   md5 '52e942bbf676267a90dd6daec35c9ced'
@@ -9,7 +9,6 @@ class Libhdf5Universal < Formula
   keg_only "HDF5 doesn't build in universal mode, so we use a hand-built binary."
 
   def install
-    # XXX - need to move this to cp.org/linked_files and make it better organized.
     puts Dir['local/HDF5-universal/*']
     prefix.install Dir['local/HDF5-universal/*']
   end
